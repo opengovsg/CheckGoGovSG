a0f9f46f640e46f753e3a66a61856d81b5c0a843
zx <67887489+tan-zx@users.noreply.github.com>
2022-11-23 14:09:53 +0800

    feat: add favicons and robots.txt

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,11 +7,48 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "android-chrome-192x192",
+      sizes: "192x192",
+      href: "/favicon/android-chrome-192x192.png",
+    },
+    {
+      rel: "android-chrome-512x512",
+      sizes: "512x512",
+      href: "/favicon/android-chrome-512x512.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/favicon/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    },
+  ];
+};
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "check.go.gov.sg",
   viewport: "width=device-width,initial-scale=1",
 });
 
