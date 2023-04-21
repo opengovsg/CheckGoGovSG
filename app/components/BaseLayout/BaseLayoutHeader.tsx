c934740227cf4dd1df73b0f33d9fed4c9344a800
zx <67887489+tan-zx@users.noreply.github.com>
2023-04-21 17:18:34 +0800

    feat: dynamic logo route

--- app/components/BaseLayout/BaseLayoutHeader.tsx
+++ app/components/BaseLayout/BaseLayoutHeader.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Flex, Img, Link } from "@chakra-ui/react";
+import type { RouteMatch } from "@remix-run/react";
+import { useMatches } from "@remix-run/react";
 import GoLogo from "app/assets/base-layout/go-main-logo.svg";
 import GoLogoMini from "app/assets/base-layout/go-main-logo-mini.svg";
 
 import { useIsSmallIcon } from "~/hooks";
 
 export const BaseLayoutHeader: React.FC = () => {
+  const matches = useMatches();
+  const { id: currentRoute } = matches.at(-1) as RouteMatch;
+  const CheckGoGovSGLogoHref =
+    currentRoute === "routes/index" ? "https://go.gov.sg" : "/";
   const isSmallIcon = useIsSmallIcon();
   return (
     <Flex
@@ -15,7 +21,7 @@ export const BaseLayoutHeader: React.FC = () => {
       px={{ base: "1.5rem", md: "5.5rem", lg: "9.25rem" }}
       py={{ base: "0.25rem", md: "2.5rem" }}
     >
-      <Link title="CheckGoGovSG logo" p="4" href="https://go.gov.sg">
+      <Link title="CheckGoGovSG logo" p="4" href={CheckGoGovSGLogoHref}>
         <Img
           src={isSmallIcon ? GoLogoMini : GoLogo}
           alt="Go Logo"
